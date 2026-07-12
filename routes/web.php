e351e61f7a5f0193f7aed64384aa13669568e5fc
Syed-muneeb-12 <techaudio12@gmail.com>
2026-07-12 15:46:03 +0500

    Refactor home route to use Route::view with parameters for greeting and person

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/',"welcome", [
+    "greeting" =>"Hello World",
+    'person' =>request('person'),
+]);
